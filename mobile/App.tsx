b84aff3c3362db01100d7ecc99fb4f644a478ef2
ErildoJS <erildofranciscojs@gmail.com>
2024-02-09 10:09:35 +0100

    chore add fonts and statusbar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter'
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold
+  })
+  if (!fontsLoaded) {
+    return (
+      <Loading />
+    )
+  }
+
   return (
-    <View style={{ flex: 1, backgroundColor: '#333', alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, backgroundColor: '#09090A', alignItems: 'center', justifyContent: 'center' }}>
+      <StatusBar style='light' backgroundColor='transparent' translucent />
       <Text style={{ color: '#FFF', fontSize: 29, fontWeight: 'bold' }}>erildo</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
