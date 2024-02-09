2993605141308b49cba9214bb70150ce430baa00
ErildoJS <erildofranciscojs@gmail.com>
2024-02-09 09:54:15 +0100

    chore add assets folder

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,8 +3,8 @@ import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View >
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={{ flex: 1, backgroundColor: '#333', alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ color: '#FFF', fontSize: 29, fontWeight: 'bold' }}>erildo</Text>
       <StatusBar style="auto" />
     </View>
   );
