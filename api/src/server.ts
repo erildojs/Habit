b7e638751a912867c0a66b5b3e41616e923e8b22
ErildoJS <erildofranciscojs@gmail.com>
2024-02-10 11:07:33 +0100

    feat: create habit route

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config'
 import Fastify from "fastify";
 import cors from "@fastify/cors";
+import { appRoutes } from './routes';
 
 const app = Fastify()
 app.register(cors)
+app.register(appRoutes)
 
 app.listen({
   port: Number(process.env.NODE_PORT)
