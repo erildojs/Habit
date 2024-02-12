445eb7bfbc04ad36243c5c711d97f62e4fb7d826
ErildoJS <erildofranciscojs@gmail.com>
2024-02-12 11:58:49 +0100

    feat: add header component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,17 @@
+import { Header } from './components/Header'
 import './styles/global.css'
 
-function App() {
+export function App() {
 
   return (
-    <h1 className=''>erildo</h1>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+
+
+      </div>
+    </div>
   )
 }
 
-export default App
+
