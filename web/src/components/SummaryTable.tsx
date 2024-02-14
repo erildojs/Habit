195ddc1268c721a310e246f4039ce07163b919f3
ErildoJS <erildofranciscojs@gmail.com>
2024-02-14 14:10:02 +0100

    feat: add modal and popover components

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,7 +1,7 @@
 import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning"
 import { HabitDay } from "./HabitDay"
 
-const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+const weekDays = ['Dom', 'Seg', 'Terç', 'Qua', 'Qui', 'Sext', 'Sab']
 const summaryDates = generateDatesFromYearBeginning()
 const minimumSumaryDatesSize = 18 * 7 //18 weeks
 const amountOfDaysToFill = minimumSumaryDatesSize - summaryDates.length
@@ -12,7 +12,7 @@ export function SummaryTable() {
       <div className="grid grid-rows-7 grid-flow-row gap-3">
         {weekDays.map((weekDay, i) => {
           return (
-            <div key={`${weekDay}-${i}`} className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center">
+            <div key={`${weekDay}-${i}`} className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center mr-4">
               {weekDay}
             </div>
           )
@@ -21,7 +21,7 @@ export function SummaryTable() {
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map(date => {
-          return <HabitDay key={date.toString()} />
+          return <HabitDay amount={5} completed={Math.round(Math.random() * 5)} key={date.toString()} />
         })}
 
         {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
