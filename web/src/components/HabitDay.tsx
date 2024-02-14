fc77757400f41937627f5ffd0afa1d5fdbd778e2
ErildoJS <erildofranciscojs@gmail.com>
2024-02-14 14:15:45 +0100

    refactor: change bg to border

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -15,11 +15,11 @@ export function HabitDay({completed, amount}: HabitDayProps) {
     <Popover.Root>
       <Popover.Trigger className={clsx('h-10 w-10 border-2 rounded-lg', {
       'bg-zinc-900 border-zinc-800': completedPercentage === 0,
-      'bg-violet-900 bg-violet-700': completedPercentage > 0 && completedPercentage < 20,
-      'bg-violet-800 bg-violet-600': completedPercentage >= 20 && completedPercentage < 40,
-      'bg-violet-700 bg-violet-500': completedPercentage >= 40 && completedPercentage < 60,
-      'bg-violet-600 bg-violet-500': completedPercentage >= 60 && completedPercentage < 80,
-      'bg-violet-500 bg-violet-400': completedPercentage >= 80,  
+      'bg-violet-900 border-violet-700': completedPercentage > 0 && completedPercentage < 20,
+      'bg-violet-800 border-violet-600': completedPercentage >= 20 && completedPercentage < 40,
+      'bg-violet-700 border-violet-500': completedPercentage >= 40 && completedPercentage < 60,
+      'bg-violet-600 border-violet-500': completedPercentage >= 60 && completedPercentage < 80,
+      'bg-violet-500 border-violet-400': completedPercentage >= 80,  
       })} />
 
       <Popover.Portal>
