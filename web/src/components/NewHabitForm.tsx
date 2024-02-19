545acd6b9645b58d7468c194eb7903d7a2f7b314
ErildoJS <erildofranciscojs@gmail.com>
2024-02-19 12:42:37 +0100

    feat: add data of forms

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,5 +1,6 @@
 import * as  Checkbox from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
+import { FormEvent, useState } from "react";
 
 const availableWeekDays = [
   'Domingo',
@@ -11,17 +12,35 @@ const availableWeekDays = [
   'Sabado'
 ]
 
+
+
 export function NewHabitForm() {
+  const [weekDays, setWeekDays] = useState<number[]>([])
+
+  function createNewHabit(event: FormEvent) {
+    
+  }
+  
+  function handleToggleWeekDay(weekDay: number) {
+    if(weekDays.includes(weekDay)) {
+      const weekDaysWithRemoveOne = weekDays.filter(day => day !== weekDay)
+      setWeekDays(weekDaysWithRemoveOne)
+    } else {
+      const weekDaysWithAddedOne = [...weekDays, weekDay]
+      setWeekDays(weekDaysWithAddedOne)
+    }
+  }
+
   return (
-    <form className="w-full flex flex-col mt-6">
+    <form onSubmit={createNewHabit} className="w-full flex flex-col mt-6">
       <label htmlFor="title" className="font-semibold leading-tight">Qual o seu comprometimento?</label>
       <input type="text" id="title" placeholder="ex.: Exercicios, dormir bem, etc..." className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400" autoFocus />
       <label htmlFor="title" className="font-semibold leading-tight mt-4">Qual a recorrencia?</label>
 
       <div className="flex flex-col gap-2 mt-3">
-        {availableWeekDays.map(weekDay => {
+        {availableWeekDays.map((weekDay, index) => {
           return (
-            <Checkbox.Root  key={weekDay} className='flex items-center gap-3 group'>
+            <Checkbox.Root key={weekDay} onCheckedChange={() => handleToggleWeekDay(index)} className='flex items-center gap-3 group'>
               <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
                 <Checkbox.Indicator>
                   <Check size={20} className='text-white' /> 
